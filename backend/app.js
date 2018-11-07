84343260a1a9c100c374e792a5a040344f6c14ab
KingOfKamenRiders <857365274@qq.com>
2018-11-07 21:17:23 +0800

    作业五

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ const mysql = require('mysql');
 const multer = require('multer');
 const app = express();
 var fs = require('fs');
+var MySQLStore = require('express-mysql-session');
+
 
 
 
@@ -19,16 +21,9 @@ let options = {
   }
 }
 app.use(express.static('uploads',options));
-app.set('trust proxy', 1);
-app.use(session({
-  secret: 'Logan best',
-  resave:false,
-  saveUninitialized:true,
-  cookie: {
-    originalMaxAge: 100000,
-    maxAge:1800000,
-  }
-}));
+
+
+
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, `./uploads/${req.session.cid}`)
@@ -47,7 +42,25 @@ connection.connect((err)=>{
     console.log("can't connect to database, please check your config");
   else
     console.log('connected as id ' + connection.threadId);
-})
+});
+var sessionStore = new MySQLStore({}/* session store options */, connection);
+
+//会话管理
+//将session数据存储在mysql数据库中(使用express-mysql-session)
+app.set('trust proxy', 1);
+app.use(session({
+  name:'happy.web',
+  secret: 'Logan best',
+  resave:false,
+  saveUninitialized:true,
+  cookie: {
+    originalMaxAge: 100000,
+    maxAge:1800000,
+    httpOnly:true,
+    secure:true,
+  },
+  store:sessionStore,
+}));
 
 //处理登陆
 app.get('/api/login',(req,res,next)=>{
@@ -61,7 +74,7 @@ app.get('/api/login',(req,res,next)=>{
       }
       else{
         if(results.length>0&&results[0].password === codedPass){
-          console.log(req.session.cid);
+          //记录session data
           req.session.cid = req.query.cid;
 
           if(!fs.existsSync(`uploads/${req.session.cid}`)){
@@ -73,16 +86,32 @@ app.get('/api/login',(req,res,next)=>{
           res.send(ResultMessage.WRONG);
       }
   })
-})
+});
+app.get('/api/logout',function (req,res,next) {
+  //删除session
+  req.session.destroy((err)=>console.log(err));
+  res.send(ResultMessage.OK);
+
+});
+app.get('/api/isLoggedIn',function (req,res,next) {
+  //删除session
+  if(req.session.cid){
+    res.send(req.session.cid);
+  }else{
+    res.send("");
+  }
+});
 app.get('/api/hello', function(req, res, next) {
+  console.log(req.session.id);
   if (req.session.counter) {
-    req.session.counter++
+    req.session.counter++;
     res.send(req.session.counter.toString());
   } else {
-    req.session.counter = 1
+    req.session.counter = 1;
     res.send('welcome to the session demo. refresh!')
   }
-})
+});
+
 //处理注册
 app.get('/api/signUp',(req,res)=>{
   let ud = new UserDAO(connection);
@@ -102,12 +131,12 @@ app.get('/api/signUp',(req,res)=>{
       res.send(ResultMessage.OK);
     }
   })
-})
+});
 
 app.post('/api/uploadImg',upload.array('hero',9),(req,res)=>{
   console.log(req.session.cid);
   res.send(ResultMessage.OK);
-})
+});
 
 app.get('/api/myHeroes',(req,res)=>{
     if(!fs.existsSync(`uploads/${req.session.cid}`)) {
@@ -120,5 +149,5 @@ app.get('/api/myHeroes',(req,res)=>{
     }
 
 })
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
--- src/api/login.js
+++ src/api/login.js
@@ -19,3 +19,14 @@ export function signUp(cid,password,callback) {
   }).then((response)=>callback(response))
     .catch((error)=>console.log(error))
 }
+export function logout(callback) {
+  axios.get('/api/logout')
+    .then((res)=>callback(res))
+    .catch((err)=>console.log(err))
+}
+
+export function isLoggedIn(callback) {
+  axios.get('/api/isLoggedIn')
+    .then((res)=>callback(res))
+    .catch((err)=>console.log(err))
+}
